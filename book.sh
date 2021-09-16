15e090c367125606971f8ef546a80cc2213c59f9
Ryan Smith <ryan.smith@ucdconnect.ie>
2021-09-16 15:50:16 +0100

    cat from password.txt

--- book.sh
+++ book.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Suspend to save state to the disk, wake at local time
-echo "PASSWORD" | sudo -S rtcwake -m disk -l -t $(date +%s -d `tomorrow 06:15`)
+cat secrets/password.txt | sudo --stdin rtcwake -m disk -l -t $(date +%s -d `tomorrow 06:15`)
 
 conda activate gymbooking
 python main.py
