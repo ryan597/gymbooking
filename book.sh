98b6ed2bd94d903ce429fe8ef49aebfc3b331d26
Ryan Smith <ryan.smith@ucdconnect.ie>
2021-09-16 16:11:36 +0100

    wake doesnt need sudo ..

--- book.sh
+++ book.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Suspend to save state to the disk, wake at local time
-cat secrets/password.txt | sudo --stdin rtcwake -m disk -l -t $(date +%s -d `tomorrow 06:15`)
+rtcwake -m disk -l -t $(date +%s -d `tomorrow 06:15`)
 
 conda activate gymbooking
 python main.py
 
-sudo rtcwake -m disk
+rtcwake -m disk
