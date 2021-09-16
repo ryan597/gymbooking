0ecbc9f0df53ba7c0ed71915ccd314dce95ab56f
Ryan Smith <ryan.smith@ucdconnect.ie>
2021-09-16 19:59:05 +0100

    sleep to mem, disk requires encryption key

--- book.sh
+++ book.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Suspend to save state to the disk, wake at local time
-cat secrets/password.txt | sudo --stdin rtcwake -m disk -l -t $(date +%s --date 'tomorrow 06:15')
+cat secrets/password.txt | sudo --stdin rtcwake -m mem -l -t $(date +%s --date 'tomorrow 06:15')
 
 conda activate gymbooking
 python main.py > errorlog.txt 2>&1 # log any errors for debugging
