6cd7ad4a6fb3e1339d97f565a8d0b07b62575a53
Ryan Smith <ryan.smith@ucdconnect.ie>
2021-09-16 10:46:37 +0100

    create env

---
+++ main.py
@@ -0,0 +1,15 @@
+
+# In the parent directory you should have a file `config.json` which is
+# structured as follows. Please fill in the student_number
+# and time_slot fields with your prefered values.
+"""
+{
+    "url" : "https://hub.ucd.ie/usis/W_HU_MENU.P_PUBLISH?p_tag=GYMBOOK",
+    "student_number" : "15311336",
+    "time_slot" : "9:30"
+}
+"""
+
+if __name__ == "__main__":
+    pass
+
